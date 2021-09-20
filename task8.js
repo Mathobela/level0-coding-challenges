da2f3dc933263ecb97307278d96bbb26dd4bfd7b
Mathobela Mawasha <mathobela.mashiane@gmail.com>
2021-09-20 13:16:02 +0200

    Task 8 corrections

--- task8.js
+++ task8.js
@@ -4,10 +4,22 @@ function convertToHoursMin (num) {
     var hours = Math.round(decimalHours);
     var decimalMinutes = (decimalHours - hours) * minInHour;
     var minutes = Math.round(decimalMinutes);
-    if (hours < 2) {
-        return hours + " hour, " + minutes + " minutes";
+    if (hours < 2 ) {
+        if (minutes > 1) {
+            return hours + " hour, " + minutes + " minutes";
+        }
+        else {
+            return hours + " hour, " + minutes + " minute";
+        }
     }
     else {
-        return hours + " hours, " + minutes + " minutes";
+        if(minutes > 1) {
+            return hours + " hours, " + minutes + " minutes";
+        }
+        else {
+            return hours + " hours, " + minutes + " minute";
+        }
+
     }
 }
+
