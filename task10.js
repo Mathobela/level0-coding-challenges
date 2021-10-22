7d33c3074b161e418aec19ea647bbf8386c56068
Mathobela Mawasha <mathobela.mashiane@gmail.com>
2021-10-22 23:55:16 +0200

    Replaced var with const and let, called function with arguments

--- task10.js
+++ task10.js
@@ -1,18 +1,18 @@
 function findCommonChar (string1, string2) {
-    var longString = Math.max(string1.length, string2.length);
-    var commonList = "";
-    var string1LowCase = string1.toLocaleLowerCase();
-    var string2LowCase = string2.toLocaleLowerCase();
-    for (var i = 0; i < longString; i++) {
+    const longString = Math.max(string1.length, string2.length);
+    let commonList = "";
+    const string1LowCase = string1.toLocaleLowerCase();
+    const string2LowCase = string2.toLocaleLowerCase();
+    for (let i = 0; i < longString; i++) {
         if (string2LowCase.includes(string1LowCase[i])) {
             if (commonList.includes(string1LowCase[i])) {
                 continue;
             }
-            commonList = commonList + string1LowCase[i] + ", ";
+            commonList += string1LowCase[i] + ", ";
         }
     }
-    var newList = commonList.trim();
-    var newList = newList.slice(0, -1);
+    let newList = commonList.trim();
+    newList = newList.slice(0, -1);
     console.log(newList);
 }
-findCommonChar();
+findCommonChar("Mathobela", "That's my name");
