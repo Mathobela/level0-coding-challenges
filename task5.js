300eab2d3ee5ef0db257a3fa42539c8af2baf497
Mathobela Mawasha <mathobela.mashiane@gmail.com>
2021-10-22 22:46:56 +0200

    Replaced let with const

--- task5.js
+++ task5.js
@@ -1,6 +1,6 @@
 function calculateArea (firstLength, secondLength, thirdLength) {
-    let semiPeri = 1/2 * (firstLength + secondLength + thirdLength);
-    let triangleArea = Math.sqrt(semiPeri * (semiPeri - firstLength) * (semiPeri - secondLength) * (semiPeri-thirdLength)); //Heron's formula
+    const semiPeri = 1/2 * (firstLength + secondLength + thirdLength);
+    const triangleArea = Math.sqrt(semiPeri * (semiPeri - firstLength) * (semiPeri - secondLength) * (semiPeri-thirdLength)); //Heron's formula
     return triangleArea;
 }
 calculateArea(3, 6, 7);
