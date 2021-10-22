36f298012e763c605b678a67d98197a6973973ed
Mathobela Mawasha <mathobela.mashiane@gmail.com>
2021-10-22 16:43:08 +0200

    Replaced var with let, called the function with arguments

--- task5.js
+++ task5.js
@@ -1,5 +1,6 @@
 function calculateArea (firstLength, secondLength, thirdLength) {
-    var semiPeri = 1/2 * (firstLength + secondLength + thirdLength);
-    var triangleArea = Math.sqrt(semiPeri * (semiPeri - firstLength) * (semiPeri - secondLength) * (semiPeri-thirdLength)); //Heron's formula
+    let semiPeri = 1/2 * (firstLength + secondLength + thirdLength);
+    let triangleArea = Math.sqrt(semiPeri * (semiPeri - firstLength) * (semiPeri - secondLength) * (semiPeri-thirdLength)); //Heron's formula
     return triangleArea;
 }
+calculateArea(3, 6, 7);
