e18a158688ccda7f3d330bea865282b07608ca93
Mathobela Mawasha <mathobela.mashiane@gmail.com>
2021-10-27 13:27:37 +0200

    Used Prettier

--- task7.js
+++ task7.js
@@ -1,11 +1,11 @@
-function convertCelsiusToFahrenheit (celsiusTemp) {
-    const FahrenheitTemp = celsiusTemp * 9/5 + 32; //metric conversion formula (celsius to fahrenheit)
-    return FahrenheitTemp;
+function convertCelsiusToFahrenheit(celsiusTemp) {
+  const FahrenheitTemp = (celsiusTemp * 9) / 5 + 32; //metric conversion formula (celsius to fahrenheit)
+  return FahrenheitTemp;
 }
 
-function convertFahrenheitToCelsius (fahrenhitTemp) {
-    const celsiusTemp = (fahrenhitTemp - 32) * 5/9;
-    return celsiusTemp;
+function convertFahrenheitToCelsius(fahrenhitTemp) {
+  const celsiusTemp = ((fahrenhitTemp - 32) * 5) / 9;
+  return celsiusTemp;
 }
 convertCelsiusToFahrenheit(32);
 convertFahrenheitToCelsius(100);
