26bff9c63e89b3a9d13865579aad05ef2b8e9bb8
Mathobela Mawasha <mathobela.mashiane@gmail.com>
2021-10-27 13:12:07 +0200

    Used Prettier

--- task9.js
+++ task9.js
@@ -1,18 +1,17 @@
-function findVowels (str) {
-    const vowels = ["a","A", "e", "E", "i", "I", "o", "O", "u", "U"];
-    let foundVowels = "";
-    for (let char of str) {
-        if (vowels.includes(char)) {
-            if (foundVowels.includes(char)) {
-                continue;
-            }
-        currentVowel = char;
-        foundVowels += char + ", ";
-        }
+function findVowels(str) {
+  const vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+  let foundVowels = "";
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      if (foundVowels.includes(char)) {
+        continue;
+      }
+      currentVowel = char;
+      foundVowels += char + ", ";
     }
-    let vowelList = foundVowels.trim();
-    vowelList = vowelList.slice(0, -1);
-    console.log(vowelList);
+  }
+  let vowelList = foundVowels.trim();
+  vowelList = vowelList.slice(0, -1);
+  console.log(vowelList);
 }
 findVowels("Paulina");
-
