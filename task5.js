e9cfec946763fbec8280ad229c9a9be0ef2f0e3e
Mathobela Mawasha <mathobela.mashiane@gmail.com>
2021-10-27 13:25:48 +0200

    Used Prettier

--- task5.js
+++ task5.js
@@ -1,6 +1,11 @@
-function calculateArea (firstLength, secondLength, thirdLength) {
-    const semiPeri = 1/2 * (firstLength + secondLength + thirdLength);
-    const triangleArea = Math.sqrt(semiPeri * (semiPeri - firstLength) * (semiPeri - secondLength) * (semiPeri-thirdLength)); //Heron's formula
-    return triangleArea;
+function calculateArea(firstLength, secondLength, thirdLength) {
+  const semiPeri = (1 / 2) * (firstLength + secondLength + thirdLength);
+  const triangleArea = Math.sqrt(
+    semiPeri *
+      (semiPeri - firstLength) *
+      (semiPeri - secondLength) *
+      (semiPeri - thirdLength)
+  ); //Heron's formula
+  return triangleArea;
 }
 calculateArea(3, 6, 7);
