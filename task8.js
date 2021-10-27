02b70da006b2c77d841977b88d30c5795a3a9ce4
Mathobela Mawasha <mathobela.mashiane@gmail.com>
2021-10-27 13:28:48 +0200

    Used Prettier

--- task8.js
+++ task8.js
@@ -1,19 +1,19 @@
-function convertToHoursMin (num) {
-    const minInHour = 60;
-    const decimalHours = num / minInHour;
-    const hours = Math.floor(decimalHours);
-    const decimalMinutes = (decimalHours - hours) * minInHour;
-    const minutes = Math.floor(decimalMinutes);
-    let hourString = "hour";
-    let minString = "minute";
-    let time = "";
-    if (hours > 1) {
-        hourString = "hours";
-    }
-    if (minutes > 1) {
-        minString = "minutes";
-    }
-    time = hours + " " + hourString + ", " + minutes + " " + minString;
-    return time;
+function convertToHoursMin(num) {
+  const minInHour = 60;
+  const decimalHours = num / minInHour;
+  const hours = Math.floor(decimalHours);
+  const decimalMinutes = (decimalHours - hours) * minInHour;
+  const minutes = Math.floor(decimalMinutes);
+  let hourString = "hour";
+  let minString = "minute";
+  let time = "";
+  if (hours > 1) {
+    hourString = "hours";
+  }
+  if (minutes > 1) {
+    minString = "minutes";
+  }
+  time = hours + " " + hourString + ", " + minutes + " " + minString;
+  return time;
 }
 convertToHoursMin(66);
